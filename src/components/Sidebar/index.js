70da423d0b65c363da061fef3fbae5ad1adfbc68
devfernandoabreu <devfernandoabreu@gmail.com>
2020-10-29 17:48:41 -0300

    Finalizando MenusLinks e iniciando MenuBar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 import Profile from "../Profile"
+import SocialLinks from "../SocialLinks"
+import MenuLinks from "../MenuLinks"
 
 import * as S from "./styled"
 
@@ -7,6 +9,8 @@ const Sidebar = () => {
   return (
     <S.SidebarWrapper>
       <Profile />
+      <SocialLinks />
+      <MenuLinks />
     </S.SidebarWrapper>
   )
 }
